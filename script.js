2b1e8c4a8ae4f2a016eac17086fa350081eb95f4
shashith-mm <skothapally52@massmutual.com>
2025-10-23 16:26:53 +0530

    added-log-statement-for-testing

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Function to check for a winner
     function checkWinner() {
+        console.log('checking winner. please wait');
         const winPatterns = [
             [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
             [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
